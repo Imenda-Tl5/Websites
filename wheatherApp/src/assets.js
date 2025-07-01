4e041c4070039b608dc1e55f1e7e1e4204b76a20
Imenda-tl5 <Imendat.com>
2025-07-01 08:06:22 +0200

    Wheatherapp posted

---
+++ wheatherApp/src/assets.js
@@ -0,0 +1,434 @@
+export const icons ={
+    foggy:"M720-200q-17 0-28.5-11.5T680-240q0-17 11.5-28.5T720-280q17 0 28.5 11.5T760-240q0 17-11.5 28.5T720-200ZM280-80q-17 0-28.5-11.5T240-120q0-17 11.5-28.5T280-160q17 0 28.5 11.5T320-120q0 17-11.5 28.5T280-80Zm-40-120q-17 0-28.5-11.5T200-240q0-17 11.5-28.5T240-280h360q17 0 28.5 11.5T640-240q0 17-11.5 28.5T600-200H240ZM400-80q-17 0-28.5-11.5T360-120q0-17 11.5-28.5T400-160h280q17 0 28.5 11.5T720-120q0 17-11.5 28.5T680-80H400ZM300-320q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z",
+    cloudy:`M504-425Zm20 385H420l20-12.5q20-12.5 43.5-28t43.5-28l20-12.5q81-6 149.5-49T805-285q-86-8-163-43.5T504-425q-61-61-97-138t-43-163q-77 43-120.5 118.5T200-444v12l-12 5.5q-12 5.5-26.5 11.5T135-403.5l-12 5.5q-2-11-2.5-23t-.5-23q0-146 93-257.5T450-840q-18 99 11 193.5T561-481q71 71 165.5 100T920-370q-26 144-138 237T524-40Zm-284-80h180q25 0 42.5-17.5T480-180q0-25-17-42.5T422-240h-52l-20-48q-14-33-44-52.5T240-360q-50 0-85 34.5T120-240q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T40-240q0-83 58.5-141.5T240-440q60 0 109.5 32.5T423-320q57 2 97 42.5t40 97.5q0 58-41 99t-99 41H240Z`,
+    rainy:"M558-84q-15 8-30.5 2.5T504-102l-60-120q-8-15-2.5-30.5T462-276q15-8 30.5-2.5T516-258l60 120q8 15 2.5 30.5T558-84Zm240 0q-15 8-30.5 2.5T744-102l-60-120q-8-15-2.5-30.5T702-276q15-8 30.5-2.5T756-258l60 120q8 15 2.5 30.5T798-84Zm-480 0q-15 8-30.5 2.5T264-102l-60-120q-8-15-2.5-30.5T222-276q15-8 30.5-2.5T276-258l60 120q8 15 2.5 30.5T318-84Zm-18-236q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z",
+    thunder:"m300-40 36-100h-76l50-140h100l-43 100h83L340-40h-40Zm270-40 28-80h-78l43-120h100l-35 80h82L610-80h-40ZM300-320q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z",
+    hail:"M200-200q-17 0-28.5-11.5T160-240q0-17 11.5-28.5T200-280q17 0 28.5 11.5T240-240q0 17-11.5 28.5T200-200Zm180 0q-17 0-28.5-11.5T340-240q0-17 11.5-28.5T380-280q17 0 28.5 11.5T420-240q0 17-11.5 28.5T380-200Zm180 0q-17 0-28.5-11.5T520-240q0-17 11.5-28.5T560-280q17 0 28.5 11.5T600-240q0 17-11.5 28.5T560-200Zm180 0q-17 0-28.5-11.5T700-240q0-17 11.5-28.5T740-280q17 0 28.5 11.5T780-240q0 17-11.5 28.5T740-200ZM120-360q-17 0-28.5-11.5T80-400q0-17 11.5-28.5T120-440q17 0 28.5 11.5T160-400q0 17-11.5 28.5T120-360Zm180 0q-17 0-28.5-11.5T260-400q0-17 11.5-28.5T300-440q17 0 28.5 11.5T340-400q0 17-11.5 28.5T300-360Zm180 0q-17 0-28.5-11.5T440-400q0-17 11.5-28.5T480-440q17 0 28.5 11.5T520-400q0 17-11.5 28.5T480-360Zm180 0q-17 0-28.5-11.5T620-400q0-17 11.5-28.5T660-440q17 0 28.5 11.5T700-400q0 17-11.5 28.5T660-360Zm180 0q-17 0-28.5-11.5T800-400q0-17 11.5-28.5T840-440q17 0 28.5 11.5T880-400q0 17-11.5 28.5T840-360ZM200-520q-17 0-28.5-11.5T160-560q0-17 11.5-28.5T200-600q17 0 28.5 11.5T240-560q0 17-11.5 28.5T200-520Zm180 0q-17 0-28.5-11.5T340-560q0-17 11.5-28.5T380-600q17 0 28.5 11.5T420-560q0 17-11.5 28.5T380-520Zm180 0q-17 0-28.5-11.5T520-560q0-17 11.5-28.5T560-600q17 0 28.5 11.5T600-560q0 17-11.5 28.5T560-520Zm180 0q-17 0-28.5-11.5T700-560q0-17 11.5-28.5T740-600q17 0 28.5 11.5T780-560q0 17-11.5 28.5T740-520ZM120-680q-17 0-28.5-11.5T80-720q0-17 11.5-28.5T120-760q17 0 28.5 11.5T160-720q0 17-11.5 28.5T120-680Zm180 0q-17 0-28.5-11.5T260-720q0-17 11.5-28.5T300-760q17 0 28.5 11.5T340-720q0 17-11.5 28.5T300-680Zm180 0q-17 0-28.5-11.5T440-720q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720q0 17-11.5 28.5T480-680Zm180 0q-17 0-28.5-11.5T620-720q0-17 11.5-28.5T660-760q17 0 28.5 11.5T700-720q0 17-11.5 28.5T660-680Zm180 0q-17 0-28.5-11.5T800-720q0-17 11.5-28.5T840-760q17 0 28.5 11.5T880-720q0 17-11.5 28.5T840-680Z",
+    mix:"M500-40q-25 0-42.5-17T440-99q0-12 4.5-23t13.5-19l42-39 42 39q9 8 13.5 19t4.5 23q0 25-17.5 42T500-40Zm-138-60-42-42 118-118 42 42-118 118Zm258-60-60-60 60-60 60 60-60 60Zm-360 0-60-60 60-60 60 60-60 60Zm40-160q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z",
+    snow:"M260-200q-21 0-35.5-14.5T210-250q0-21 14.5-35.5T260-300q21 0 35.5 14.5T310-250q0 21-14.5 35.5T260-200ZM380-80q-21 0-35.5-14.5T330-130q0-21 14.5-35.5T380-180q21 0 35.5 14.5T430-130q0 21-14.5 35.5T380-80Zm120-120q-21 0-35.5-14.5T450-250q0-21 14.5-35.5T500-300q21 0 35.5 14.5T550-250q0 21-14.5 35.5T500-200Zm240 0q-21 0-35.5-14.5T690-250q0-21 14.5-35.5T740-300q21 0 35.5 14.5T790-250q0 21-14.5 35.5T740-200ZM620-80q-21 0-35.5-14.5T570-130q0-21 14.5-35.5T620-180q21 0 35.5 14.5T670-130q0 21-14.5 35.5T620-80ZM300-360q-91 0-155.5-64.5T80-580q0-83 55-145t136-73q32-57 87.5-89.5T480-920q90 0 156.5 57.5T717-719q69 6 116 57t47 122q0 75-52.5 127.5T700-360H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-744l-10 24h-25q-57 2-97.5 42.5T160-580q0 58 41 99t99 41Zm180-100Z"
+}
+const cities_list = `name
+Abidjan
+Abu Dhabi
+Abuja
+Accra
+Addis Ababa
+Ahmedabad
+Aleppo
+Alexandria
+Algiers
+Almaty
+Amman
+Amsterdam
+Anchorage
+Andorra la Vella
+Ankara
+Antananarivo
+Apia
+Arnold
+Ashgabat
+Asmara
+Asuncion
+Athens
+Auckland
+Avarua
+Baghdad
+Baku
+Bamako
+Banda Aceh
+Bandar Seri Begawan
+Bandung
+Bangkok
+Bangui
+Banjul
+Barcelona
+Barranquilla
+Basrah
+Basse-Terre
+Basseterre
+Beijing
+Beirut
+Bekasi
+Belem
+Belgrade
+Belmopan
+Belo Horizonte
+Bengaluru
+Berlin
+Bern
+Bishkek
+Bissau
+Bogota
+Brasilia
+Bratislava
+Brazzaville
+Bridgetown
+Brisbane
+Brussels
+Bucharest
+Budapest
+Buenos Aires
+Bujumbura
+Bursa
+Busan
+Cairo
+Cali
+Caloocan
+Camayenne
+Canberra
+Cape Town
+Caracas
+Casablanca
+Castries
+Cayenne
+Charlotte Amalie
+Chengdu
+Chennai
+Chicago
+Chisinau
+Chittagong
+Chongqing
+Colombo
+Conakry
+Copenhagen
+Cordoba
+Curitiba
+Daegu
+Daejeon
+Dakar
+Dallas
+Damascus
+Dar es Salaam
+Delhi
+Denver
+Dhaka
+Dili
+Djibouti
+Dodoma
+Doha
+Dongguan
+Douala
+Douglas
+Dubai
+Dublin
+Durban
+Dushanbe
+Faisalabad
+Fort-de-France
+Fortaleza
+Freetown
+Fukuoka
+Funafuti
+Gaborone
+George Town
+Georgetown
+Gibraltar
+Gitega
+Giza
+Guadalajara
+Guangzhou
+Guatemala City
+Guayaquil
+Gujranwala
+Gustavia
+Gwangju
+Hamburg
+Hanoi
+Harare
+Havana
+Helsinki
+Ho Chi Minh City
+Hong Kong
+Honiara
+Honolulu
+Houston
+Hyderabad
+Hyderabad
+Ibadan
+Incheon
+Isfahan
+Islamabad
+Istanbul
+Izmir
+Jaipur
+Jakarta
+Jeddah
+Jerusalem
+Johannesburg
+Juarez
+Juba
+Kabul
+Kaduna
+Kampala
+Kano
+Kanpur
+Kaohsiung
+Karachi
+Karaj
+Kathmandu
+Kawasaki
+Kharkiv
+Khartoum
+Khulna
+Kigali
+Kingsburg
+Kingston
+Kingstown
+Kinshasa
+Kobe
+Kolkata
+Kota Bharu
+Kowloon
+Kuala Lumpur
+Kumasi
+Kuwait
+Kyiv
+Kyoto
+La Paz
+Lagos
+Lahore
+Libreville
+Lilongwe
+Lima
+Lisbon
+Ljubljana
+Lome
+London
+Los Angeles
+Luanda
+Lubumbashi
+Lusaka
+Luxembourg
+Macau
+Madrid
+Majuro
+Makassar
+Malabo
+Male
+Mamoudzou
+Managua
+Manama
+Manaus
+Manila
+Maputo
+Maracaibo
+Maracay
+Mariehamn
+Marigot
+Maseru
+Mashhad
+Mbabane
+Mecca
+Medan
+Medellin
+Medina
+Melbourne
+Mexico City
+Miami
+Minsk
+Mogadishu
+Monaco
+Monrovia
+Montevideo
+Montreal
+Moroni
+Moscow
+Mosul
+Multan
+Mumbai
+Muscat
+N'Djamena
+Nagoya
+Nairobi
+Nanchong
+Nanjing
+Nassau
+Nay Pyi Taw
+New York
+Niamey
+Nicosia
+Nouakchott
+Noumea
+Novosibirsk
+Nuku'alofa
+Nur-Sultan
+Nuuk
+Oranjestad
+Osaka
+Oslo
+Ottawa
+Ouagadougou
+Pago Pago
+Palembang
+Palo Alto
+Panama
+Papeete
+Paramaribo
+Paris
+Perth
+Philadelphia
+Phnom Penh
+Phoenix
+Podgorica
+Port Louis
+Port Moresby
+Port of Spain
+Port-Vila
+Port-au-Prince
+Porto Alegre
+Porto-Novo
+Prague
+Praia
+Pretoria
+Pristina
+Puebla
+Pune
+Pyongyang
+Quezon City
+Quito
+Rabat
+Rawalpindi
+Recife
+Reykjavik
+Riga
+Rio de Janeiro
+Riyadh
+Road Town
+Rome
+Roseau
+Saint George's
+Saint Helier
+Saint John's
+Saint Peter Port
+Saint Petersburg
+Saint-Denis
+Saint-Pierre
+Saipan
+Salvador
+San Antonio
+San Diego
+San Francisco
+San Jose
+San Juan
+San Marino
+San Salvador
+Sanaa
+Santa Cruz de la Sierra
+Santiago
+Santo Domingo
+Sao Paulo
+Sao Tome
+Sapporo
+Sarajevo
+Seattle
+Semarang
+Seoul
+Shanghai
+Sharjah
+Shenzhen
+Singapore
+Skopje
+Sofia
+South Tangerang
+Soweto
+Stockholm
+Sucre
+Surabaya
+Surat
+Suva
+Sydney
+Tabriz
+Taipei
+Tallinn
+Tangerang
+Tarawa
+Tashkent
+Tbilisi
+Tegucigalpa
+Tehran
+Tel Aviv
+Thimphu
+Tianjin
+Tijuana
+Tirana
+Tokyo
+Toronto
+Torshavn
+Tripoli
+Tunis
+Ulan Bator
+Vaduz
+Valencia
+Valletta
+Vancouver
+Victoria
+Vienna
+Vientiane
+Vilnius
+Warsaw
+Washington
+Wellington
+Willemstad
+Windhoek
+Wuhan
+Xi'an
+Yamoussoukro
+Yangon
+Yaounde
+Yekaterinburg
+Yerevan
+Yokohama
+Zagreb
+`
+export const Cities_list = cities_list.split(`//`)
+
+
+
+
+
+// {
+//     "coord": {
+//       "lon": 28.2871,
+//       "lat": -15.4067
+//     },
+//     "weather": [
+//       {
+//         "id": 800,
+//         "main": "Clear",
+//         "description": "clear sky",
+//         "icon": "01n"
+//       }
+//     ],
+//     "base": "stations",
+//     "main": {
+//       "temp": 293,
+//       "feels_like": 292.53,
+//       "temp_min": 293,
+//       "temp_max": 293,
+//       "pressure": 1014,
+//       "humidity": 57,
+//       "sea_level": 1014,
+//       "grnd_level": 876
+//     },
+//     "visibility": 10000,
+//     "wind": {
+//       "speed": 2.42,
+//       "deg": 102,
+//       "gust": 6.09
+//     },
+//     "clouds": {
+//       "all": 6
+//     },
+//     "dt": 1747671679,
+//     "sys": {
+//       "country": "ZM",
+//       "sunrise": 1747628553,
+//       "sunset": 1747669462
+//     },
+//     "timezone": 7200,
+//     "id": 909137,
+//     "name": "Lusaka",
+//     "cod": 200
+//   }
+
+
